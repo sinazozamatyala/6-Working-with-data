8044b4d3d6a401af37a26934a2d948aacaa8ae8a
Lenovo-User <Lenovo-User@DESKTOP-JS25F1I>
2024-01-18 21:55:16 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnStart = New Button()
+        SuspendLayout()
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(191, 75)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(94, 29)
+        btnStart.TabIndex = 0
+        btnStart.Text = "Start Here"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.Happy_birthday
+        ClientSize = New Size(837, 499)
+        Controls.Add(btnStart)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnStart As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim YourMessage As String
+        Dim BirthYear As Integer
+        YourMessage = "Happy bday! How old are you now?"
+
+        MessageBox.Show(YourMessage)
+        BirthYear = Val(InputBox("Enter Year of birth"))
+        MessageBox.Show("You are now " & 2023 - BirthYear & " years old")
+    End Sub
+End Class
